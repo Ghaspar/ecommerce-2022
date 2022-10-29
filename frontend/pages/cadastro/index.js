3d18a386e69f16b5e56972b9f2660e254ce2ac64
Lucas <lucas.ghaspar@gmail.com>
2022-10-29 15:00:30 -0300

    feat: title

---
+++ frontend/pages/cadastro/index.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Cadastro() {
+  return (
+      <>
+      <section className="signup">
+        <div className="container">
+            <div className="title">
+                <Image
+                width='50'
+                height='50'
+                alt='Market Cubos'
+                src='/images/logo.png'
+                />
+
+                <h1>
+                    Market Cubos
+                </h1>
+            </div>
+            <form action="">
+
+            </form>
+        </div>
+        
+        
+
+      </section>
+
+      </>
+  )
+}
