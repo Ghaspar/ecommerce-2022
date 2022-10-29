4afc89a8513905d6809f971fdd41a82cd2310436
Lucas <lucas.ghaspar@gmail.com>
2022-10-29 14:41:54 -0300

    chore: make structure for pages

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,10 +4,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
       <>
-
-      <h1>
-        home
-      </h1>
+        <h1>
+          home
+        </h1>
       </>
   )
 }
